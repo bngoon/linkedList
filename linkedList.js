19380d4ce00009de85dd8a15ae5c7283996ca846
Booker <bookerngoon@gmail.com>
2024-08-03 15:08:48 -0400

    practice

--- linkedList.js
+++ linkedList.js
@@ -1,7 +1,7 @@
 class Node {
     constructor(data){
         this.data = data;
-        this.next = null;
+        this.next = null; //the tail points to null
     }
 }
 
@@ -28,4 +28,18 @@ class LinkedLIst {
         }
         current.next = newNode
     }
+
+     // Delete a node given only a reference to that node
+     deleteNode(node) {
+      if (node === null || node.next === null) {// checks if node is null or if it's the last node in the list, if it is the last node or null cannot delete
+          throw new Error("Cannot delete this node.");
+      }
+      // Copy the value from the next node into the current node
+      node.data = node.next.data;
+      // Update the 'next' pointer to bypass the next node
+      node.next = node.next.next;
+  }
 }
+
+
+
